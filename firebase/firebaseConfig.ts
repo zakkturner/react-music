da0335d8a35f694512e663313a5b0f682a1f0b81
zakkturner <30674356+zakkturner@users.noreply.github.com>
2021-07-28 21:34:49 -0400

    first commit

---
+++ firebase/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyARItl9-XSQkzkesJ6bBcfiwhPyPnq9p0c",
+    authDomain: "react-music-4b5d3.firebaseapp.com",
+    projectId: "react-music-4b5d3",
+    storageBucket: "react-music-4b5d3.appspot.com",
+    messagingSenderId: "834732611719",
+    appId: "1:834732611719:web:e5e4a8869df2d2654e4022",
+    measurementId: "G-YEQH362Z7V"
+  };
---
+++ firebase/initFirebase.ts
@@ -0,0 +1,23 @@
+import firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_API_ID,
+};
+// Initialize Firebase
+// firebase.analytics();
+
+export default function initFirebase() {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  console.log("Firebase working");
+}
